15c05d7bfd37d55305ca407b8acdb01176ef35bd
Attila Patel <attila_patel@hotmail.com>
2021-11-05 19:32:07 +0000

    3rd test

--- main.tf
+++ main.tf
@@ -138,7 +138,7 @@ resource "azurerm_storage_account" "mystorageaccount" {
 # Create (and display) an SSH key
 resource "tls_private_key" "example_ssh" {
   algorithm = "RSA"
-  rsa_bits = 4096
+  rsa_bits = 2048
 }
 output "tls_private_key" { value = tls_private_key.example_ssh.private_key_pem }
 
